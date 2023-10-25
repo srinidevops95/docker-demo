90719f71d47ed0fa4c86c24bf6c4bec9f425509d
root <root@mygitlab.mobileum.com>
2023-10-25 14:28:49 +0530

    adding docker contenets to repo

---
+++ docker/compose/flask-app/users.sh
@@ -0,0 +1,4 @@
+curl --header "Content-Type: application/json" --request POST --data '{"name":"Srini"}' localhost:80
+curl --header "Content-Type: application/json" --request POST --data '{"name":"Habeeb"}' localhost:80
+curl --header "Content-Type: application/json" --request POST --data '{"name":"Bjorn"}' localhost:80
+curl --header "Content-Type: application/json" --request POST --data '{"name":"Sankar"}' localhost:80
